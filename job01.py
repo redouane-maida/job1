7dff0f10b881b65b30b008231c5cf4dd6a25435b
redouane-maida <redouane.maida@laplateforme.io>
2024-10-21 12:30:36 +0200

    Premier Projet

---
+++ job01.py
@@ -0,0 +1,7 @@
+
+valeur1 = input("Veuillez entrer la première valeur : ")
+valeur2 = input("Veuillez entrer la deuxième valeur : ")
+if valeur1 == valeur2:
+    print("Valeur1 est égal à valeur2")
+else:
+    print("Les deux valeurs ne sont pas égales")
